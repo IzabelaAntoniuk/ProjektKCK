8d4b1e8e448a62279e9b262815b79e313a17b1d5
Izabela Antoniuk <antoniukizabela@gmail.com>
2016-10-19 15:35:52 +0200

    Dodaję menu

--- ProjektKCK/Program.cs
+++ ProjektKCK/Program.cs
@@ -8,7 +8,27 @@ namespace ProjektKCK
 {
     class Program
     {
-        static void Main(string[] args)
+        static int Menu(string[] inArray)
+        {
+            for (int i = 0; i < inArray.Length; i++)
+            {
+                if (i == selectedItem)
+                {//This section is what highlights the selected item
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Console.WriteLine("-" + inArray[i]);
+                    Console.ResetColor();
+                }
+                else
+                {//this section is what prints unselected items
+                    Console.WriteLine("-" + inArray[i]);
+                }
+            }
+
+            bottomOffset = Console.CursorTop;
+        }
+
+            static void Main(string[] args)
         {
         }
     }
