a0bae659491ac7d95ad38446666ad16b2c22aa2b
Ewa Korszaczuk <ewakorszaczuk@vp.pl>
2016-10-19 13:28:17 +0200

    user

---
+++ ProjektKCK/User.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjektKCK
+{
+    class User
+    {
+        String imie { get; set; }
+        String nazwisko { get; set; }
+        String dataUr { get; set; }
+        float waga { get; set; }
+        float wzrost { get; set; }
+        float aktywnosc { get; set; }
+    }
+}
